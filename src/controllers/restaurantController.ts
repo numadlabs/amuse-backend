e1e72974193133a3cd483e0a12fcd16e9c86787a
Gombo-Ochir Ariunbold <gombochir@Gombo-Ochirs-MacBook-Pro.local>
2024-04-29 15:52:37 +0800

    fixed getRestaurantById

--- src/controllers/restaurantController.ts
+++ src/controllers/restaurantController.ts
@@ -294,6 +294,9 @@ export const restaurantController = {
         req.user.id
       );
 
+      if (restaurant.visitCount === null && restaurant.isOwned === true)
+        restaurant.visitCount = 0;
+
       return res.status(200).json({ success: true, restaurant: restaurant });
     } catch (e) {
       next(e);
