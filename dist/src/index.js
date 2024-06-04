90ecd54b811eb02e7b4a97e1749aea3b6c403e63
Gombo-Ochir Ariunbold <gombochir@Gombo-Ochirs-MacBook-Pro.local>
2024-06-04 14:12:15 +0800

    dist cleanup

--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require("dotenv").config();
-const port = process.env.PORT || 3001;
-const server = require("./app");
-server.listen(port, () => {
-  console.log(`Server has started on port: ${port}`);
-});
--- dist/src/middlewares/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = void 0;
-function notFound(req, res, next) {
-    res.status(404);
-    const error = new Error(`Not found - ${req.originalUrl}`);
-    next(error);
-}
-exports.notFound = notFound;
