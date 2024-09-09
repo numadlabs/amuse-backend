ec1e44989ce36913e3e6b4afa97af5b6b8703610
Dalai1002 <dalai.dev1002@gmail.com>
2024-09-09 21:49:30 +0800

    update userSchema for removing data

--- src/validations/userSchema.ts
+++ src/validations/userSchema.ts
@@ -7,33 +7,52 @@ export const updateUserInfoSchema = z
       .trim()
       .max(30, "Nickname must be at most 30 characters.")
       .optional(),
-    countryId: z.string().optional(),
+    countryId: z
+      .string()
+      .transform((val) => (val === "" ? null : val))
+      .nullable()
+      .refine(
+        (val) =>
+          val === null ||
+          /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(
+            val!
+          ),
+        {
+          message: "Invalid UUID",
+        }
+      )
+      .optional(),
     birthYear: z
       .string()
-      .trim()
-      .transform((val) => parseInt(val, 10))
-      .refine((val) => !isNaN(val), {
-        message: "Invalid number format",
-      })
-      .refine((val) => val >= 1900 && val <= 2024, {
-        message: "Month must be between 1 and 12",
-      })
+      .transform((val) => (val === "" ? null : parseInt(val, 10)))
+      .nullable()
+      .refine(
+        (val) =>
+          val === null ||
+          (typeof val === "number" &&
+            val > 1900 &&
+            val < new Date().getFullYear()),
+        {
+          message: "Invalid birth year",
+        }
+      )
       .optional(),
     birthMonth: z
       .string()
-      .trim()
-      .transform((val) => parseInt(val, 10))
-      .refine((val) => !isNaN(val), {
-        message: "Invalid number format",
-      })
-      .refine((val) => val >= 1 && val <= 12, {
-        message: "Month must be between 1 and 12",
-      })
+      .transform((val) => (val === "" ? null : parseInt(val, 10)))
+      .nullable()
+      .refine(
+        (val) =>
+          val === null || (typeof val === "number" && val >= 1 && val <= 12),
+        {
+          message: "Invalid birth month",
+        }
+      )
       .optional(),
     profilePicture: z
       .string()
       .trim()
-      .max(100, "Profile picture must be at most 100 characters.")
+      .max(1, "Profile picture must be at most 1 characters.")
       .optional(),
   })
   .strict("Unexpected field detected.");
